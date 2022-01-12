9b07bcd6730b1cfd24e08380e63623da10dca6da
Theertha Vijayan <theerthavijayan96@gmail.com>
2022-01-12 15:10:47 +0530

    heroku files added

--- sample/settings.py
+++ sample/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -131,3 +133,6 @@ EMAIL_HOST_USER = 'theerthavijayan96@gmail.com'
 EMAIL_HOST_PASSWORD = 'ujcniieuoiuladjq'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+
+
